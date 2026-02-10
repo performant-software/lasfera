3dcba7e60c3e8d0f98cd082a05530b8cf607e258
Ben Silverman <ben@performantsoftware.com>
2026-02-10 17:32:12 -0500

    Attempt without skip_diff (#8)

--- manuscript/resources.py
+++ manuscript/resources.py
@@ -294,7 +294,6 @@ class LocationAliasResource(resources.ModelResource):
         import_id_fields = ["location", "placename_alias"]
         skip_unchanged = True
         report_skipped = True
-        skip_diff = True
 
     # cache manuscripts by siglum to prevent unnecessary sql queries
     _ms_cache = None
